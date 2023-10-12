f7efce6f60b5040d9e70736b4fa54e1d455eb47f
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-10-11 21:04:00 -0300

    update page login, add styles

--- src/assets/Components/Login/Login.jsx
+++ src/assets/Components/Login/Login.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
-import {Navigate, Route, Routes} from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import { UserContext } from '../../../UserStorage';
+import styles from '../../Css/Login.module.css';
 
 function Login() {
-  const {login} = React.useContext(UserContext);
+  const { login } = React.useContext(UserContext);
 
-  if(login == true) return <Navigate to='/conta' />
+  if (login == true) return <Navigate to='/conta' />
 
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<LoginForm />} />
-        <Route path='/create' element={<LoginCreate />} />
-        <Route path='/passwordLost' element={<LoginPasswordLost />} />
-        <Route path='/passwordReset' element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path='/' element={<LoginForm />} />
+          <Route path='/create' element={<LoginCreate />} />
+          <Route path='/passwordLost' element={<LoginPasswordLost />} />
+          <Route path='/passwordReset' element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   )
 }
 
