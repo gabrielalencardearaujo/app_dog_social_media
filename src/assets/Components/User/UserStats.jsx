a979872e9482832368187492ee1fb5a394fc2366
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-10-18 16:55:25 -0300

    add page stats and graphs of stats

--- src/assets/Components/User/UserStats.jsx
+++ src/assets/Components/User/UserStats.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
-import Head from '../Errors/Head'
+import React from 'react';
+import Head from '../Errors/Head';
+import { useFetcher, useFetchers } from 'react-router-dom';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../../api';
+import Loading from '../Errors/Loading';
+import Error from '../Errors/Error';
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'));
 
 function UserStats() {
-  return (
-    <div>
-      <Head title='Estatisticas' />
-      UserStats
-    </div>
-  )
+  const { data, error, request, loading } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+
+      await request(url, options)
+    }
+    getData()
+  }, [request])
+
+  if (loading) return <Loading />
+  if (error) return <Error />
+
+  if (data)
+    return (
+      <React.Suspense
+        fallback={<div>Nenhum resultado para mostrar</div>}>
+
+        <Head title='Estatisticas' />
+        <UserStatsGraphs data={data} />
+        
+      </React.Suspense>
+    )
+  else
+    return null;
 }
 
 export default UserStats
